447cadb81bf47098592ae35abf087ea930ce5d5b
Juozas <juozas@unity3d.com>
2021-01-29 22:37:41 +0200

    Equipment Slots added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     Stats playerStats;
+
+    class EquipmentSlot
+    {
+        private List<Item> equippedItems = new List<Item>();
+
+        public void AddItem(Item item)
+        {
+            if (item != null)
+                equippedItems.Add(item);
+        }
+
+        public Item RemoveItem()
+        {
+            if (equippedItems.Count > 0)
+            {
+                int lastItem = equippedItems.Count - 1;
+                Item toReturn = equippedItems[lastItem];
+                equippedItems.RemoveAt(lastItem);
+                return toReturn;
+            }
+            else return null;
+
+        }
+    }
+    
+    Dictionary<Common.CharacterItemSlots, EquipmentSlot> CharacterEquipment = new Dictionary<Common.CharacterItemSlots,EquipmentSlot>();
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +52,15 @@ public class Player : MonoBehaviour
         
     }
     
-    public void AddItem(Common.CharacterItemSlots slot, Item item)
+    public void AddItem(Common.CharacterItemSlots targetSlot, Item item)
     {
-        
+        EquipmentSlot slot = CharacterEquipment[targetSlot];
+        slot.AddItem(item);
     }
 
-    public void RemoveItem(Item item)
+    public Item RemoveItem(Common.CharacterItemSlots targetSlot)
     {
-        
+        EquipmentSlot slot = CharacterEquipment[targetSlot];
+        return slot.RemoveItem();
     }
 }
