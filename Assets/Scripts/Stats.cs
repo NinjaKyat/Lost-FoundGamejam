0f5fc8d5359ca0d32d15c6651ec088132ca345cb
NinjaKyat <vil.prakapas@gmail.com>
2021-01-29 21:53:56 +0200

    Add a stupid stats thing

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stats
+{
+    Dictionary<string, int> internalStats = new Dictionary<string, int>();
+
+    public int GetStat(string stat)
+    {
+        if (internalStats.ContainsKey(stat))
+        {
+            return internalStats[stat];
+        }
+        return 0;
+    }
+
+    public int GetMaxStat(string stat)
+    {
+        var maxStatKey = stat + "Max";
+        return GetStat(maxStatKey);
+    }
+
+    public void AddStat(string stat, int amount)
+    {
+        if (internalStats.ContainsKey(stat))
+        {
+            internalStats[stat] += amount;
+        }
+        else
+        {
+            internalStats[stat] = amount;
+        }
+    }
+}
