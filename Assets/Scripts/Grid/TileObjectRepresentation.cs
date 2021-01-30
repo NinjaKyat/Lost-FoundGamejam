e2bc7c3647b85ae367e8b1dba353d945e36f336d
Dominykas Kiauleikis <domukas64@gmail.com>
2021-01-30 16:46:58 +0200

    Fix error :^)

--- Assets/Scripts/Grid/TileObjectRepresentation.cs
+++ Assets/Scripts/Grid/TileObjectRepresentation.cs
@@ -10,14 +10,14 @@ public class TileObjectRepresentation : MonoBehaviour
     public void Spawn(Tile tile)
     {
         Instantiate(this, tile.WorldPosition, Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * Vector2Int.left, Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * Vector2Int.right, Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * Vector2Int.up, Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * Vector2Int.down, Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * (Vector2Int.left + Vector2Int.up), Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * (Vector2Int.left + Vector2Int.down), Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * (Vector2Int.right + Vector2Int.up), Quaternion.identity).Initialize(tile);
-        Instantiate(this, tile.WorldPosition + grid.Size * (Vector2Int.left + Vector2Int.down), Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * Vector2Int.left, Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * Vector2Int.right, Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * Vector2Int.up, Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * Vector2Int.down, Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * (Vector2Int.left + Vector2Int.up), Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * (Vector2Int.left + Vector2Int.down), Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * (Vector2Int.right + Vector2Int.up), Quaternion.identity).Initialize(tile);
+        Instantiate(this, tile.WorldPosition + tile.Grid.Size * (Vector2Int.left + Vector2Int.down), Quaternion.identity).Initialize(tile);
     }
 
     void Initialize(Tile tile)
