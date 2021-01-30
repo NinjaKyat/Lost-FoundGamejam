519083241aa62f8f0f7a3082a9fa93b394101f35
Dominykas Kiauleikis <domukas64@gmail.com>
2021-01-30 15:23:11 +0200

    Tiles are in

--- Assets/Dominykas/GridGenerator.cs
+++ Assets/Dominykas/GridGenerator.cs
@@ -6,30 +6,64 @@ public class GridGenerator : MonoBehaviour
 {
     public Vector2Int size;
     public TileObjectRepresentation treePrefab;
+    public TileObjectRepresentation grassPrefab;
+    public TileObjectRepresentation sandPrefab;
 
     Grid grid;
 
     void Awake()
     {
         grid = new Grid(size);
+        grid.ForEachTile(PlaceGround);
         grid.ForEachTile(PlaceTrees);
         grid.ForEachTile(PlaceObjects);
     }
 
     void PlaceTrees(Vector2Int position, ref Tile tile)
     {
-        var scaledPos = new Vector2(position.x * 0.69f, position.y * 0.35f) * 0.2f;
-        if (Mathf.PerlinNoise(scaledPos.x, scaledPos.y) > 0.5f)
-            tile.contents.Add(new TileObject(TileObject.Type.Tree));
+        if (tile.Contains(x => x is TileObject ob && ob.type == TileObject.Type.Grass))
+        {
+            var scaledPos = new Vector2(position.x * 0.69f, position.y * 0.35f) * 0.2f;
+            if (Mathf.PerlinNoise(scaledPos.x, scaledPos.y) > 0.5f)
+                tile.AddContent(new TileObject(TileObject.Type.Tree));
+        }
+    }
+
+    void PlaceGround(Vector2Int position, ref Tile tile)
+    {
+        var scaledPos = new Vector2(position.x * 0.69f + 1337f, position.y * 0.35f + 0.69f) * 0.2f;
+        var placedTile = TileObject.Type.Grass;
+        var noiseValue = Mathf.PerlinNoise(scaledPos.x, scaledPos.y);
+        if (noiseValue <= 0.2f)
+            placedTile = TileObject.Type.Sand;
+        //if (noiseValue <= 0.1f)
+        //    placedTile = TileObject.Type.Water;
+
+
+        tile.AddContent(new TileObject(placedTile));
     }
 
     void PlaceObjects(Vector2Int position, ref Tile tile)
     {
-        foreach (var contents in tile.contents)
+        foreach (var contents in tile.Contents)
         {
             if (contents is TileObject tileObject)
-                if (tileObject.type == TileObject.Type.Tree)
-                    treePrefab.Spawn(tile);
+                switch (tileObject.type)
+                {
+                    case TileObject.Type.Water:
+                        break;
+                    case TileObject.Type.Sand:
+                        sandPrefab.Spawn(tile);
+                        break;
+                    case TileObject.Type.Grass:
+                        grassPrefab.Spawn(tile);
+                        break;
+                    case TileObject.Type.Tree:
+                            treePrefab.Spawn(tile);
+                        break;
+                    default:
+                        break;
+                }
         }
     }
 }
--- Assets/Dominykas/Tile.cs
+++ Assets/Dominykas/Tile.cs
@@ -1,24 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
-public interface ITileContents
+public interface ITileContent
 {
 }
 
-public struct TileEvent : ITileContents
+public struct TileEvent : ITileContent
 {
     public GameEvent gameEvent;
 }
 
-public struct TileObject : ITileContents
+public struct TileObject : ITileContent
 {
     public enum Type
     {
-        Tree,
         Water,
-        Ground,
+        Sand,
+        Grass,
+        Tree,
     }
     public Type type;
     public TileObject(Type type)
@@ -33,12 +35,33 @@ public struct Tile
     public Vector2Int Position { get; private set; }
     public Vector2 WorldPosition => Position - Grid.Size / 2;
 
-    public List<ITileContents> contents;
+    List<ITileContent> contents;
+    public IReadOnlyList<ITileContent> Contents => contents;
 
     public Tile(Grid grid, Vector2Int position)
     {
         Grid = grid;
         Position = position;
-        contents = new List<ITileContents>();
+        contents = new List<ITileContent>();
+    }
+
+    public void AddContent(ITileContent newContents)
+    {
+        contents.Add(newContents);
+    }
+
+    public void RemoveTopContent()
+    {
+        contents.RemoveAt(contents.Count - 1);
+    }
+
+    public bool Contains(Func<ITileContent, bool> searchFunc)
+    {
+        foreach (var content in contents)
+        {
+            if (searchFunc.Invoke(content))
+                return true;
+        }
+        return false;
     }
 }
