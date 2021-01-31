9e3b3afbdb9ff796422fd165b782a2bd56527096
JuozasK <juozas@unity3d.com>
2021-01-31 13:42:17 +0200

    Get All Yellow Magic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject playerPrefab;
+    private UnityEngine.UI.Image image;
+    private float fadeSpeed = 0.7f;
     void Start()
     {
+        image = transform.GetComponentInChildren<UnityEngine.UI.Image>();
     }
 
     public void Respawn()
     {
-        Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        StartCoroutine(FadeBetween());
     }
+
+    IEnumerator FadeBetween()
+    {
+        while (image.color.a < 1)
+        {
+            float alpha = image.color.a + Time.fixedDeltaTime * fadeSpeed;
+            image.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        while (image.color.a > 0)
+        {
+            float alpha = image.color.a - Time.fixedDeltaTime * fadeSpeed;
+            image.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+    }
+    
 }
