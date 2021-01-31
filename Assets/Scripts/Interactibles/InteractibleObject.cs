184b3d8769f8b1194899981e5839691660229a9e
JuozasK <juozas@unity3d.com>
2021-01-31 14:29:28 +0200

    Trees cutting down

--- Assets/Scripts/Interactibles/InteractibleObject.cs
+++ Assets/Scripts/Interactibles/InteractibleObject.cs
@@ -7,6 +7,8 @@ public class InteractibleObject : MonoBehaviour, IInteractible
     [SerializeField, HideInInspector] Collider2D _collider;
     public Collider2D Collider => _collider;
 
+    public int health = 1;
+
     [SerializeField, HideInInspector] TileObjectRepresentation tileObject;
 
     private void OnValidate()
@@ -19,6 +21,11 @@ public class InteractibleObject : MonoBehaviour, IInteractible
 
     public void Interact(Player interactingPlayer)
     {
-        tileObject.Remove();
+        if (interactingPlayer.playerStats.GetStat("woodcutting") > 0)
+            health -= 10;
+        else health--;
+        transform.localScale = transform.localScale / 1.2f;
+        if (health < 0)
+            tileObject.Remove();
     }
 }
